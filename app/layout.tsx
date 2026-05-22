097de161d59b7ca541f5534ed4f0eefa73bd6348
PolCruz <pcf1987@gmail.com>
2026-05-22 21:27:54 +0300

    act

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,37 +1,30 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 import { Inter_Tight } from "next/font/google";
+import "./globals.css";
+
 const interTight = Inter_Tight({
   subsets: ["latin"],
   variable: "--font-inter-tight",
   display: "swap",
 });
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata: Metadata = {
   metadataBase: new URL("https://matchpolitico.com"),
-
   title: "Match Político",
-  description: "Haz el test ideológico de Match Político y descubre tu perfil político real y el partido más afín en 2 minutos.",
-
+  description:
+    "Haz el test ideológico de Match Político y descubre tu perfil político real y el partido más afín en 2 minutos.",
+  icons: {
+    icon: "/favicon.ico",
+  },
   openGraph: {
     title: "Match Político",
-    description: "Haz el test y descubre tu perfil ideológico, tu afinidad política y qué partido encaja más contigo.",
+    description:
+      "Haz el test y descubre tu perfil ideológico, tu afinidad política y qué partido encaja más contigo.",
     url: "https://matchpolitico.com",
     siteName: "Match Político",
     images: [
       {
-        url: "https://matchpolitico.com/og-image.jpg", // 👈 ABSOLUTA
+        url: "https://matchpolitico.com/og-image.jpg",
         width: 1200,
         height: 630,
         alt: "Match Político",
@@ -40,22 +33,16 @@ export const metadata: Metadata = {
     locale: "es_ES",
     type: "website",
   },
-
   twitter: {
     card: "summary_large_image",
     title: "Match Político",
     description: "Descubre tu perfil ideológico",
     images: ["https://matchpolitico.com/og-image.jpg"],
   },
-
-  icons: {
-  icon: "/favicon.ico",
-},
-alternates: {
-  canonical: "https://matchpolitico.com",
-},
+  alternates: {
+    canonical: "https://matchpolitico.com",
+  },
 };
-
 
 export default function RootLayout({
   children,
@@ -63,12 +50,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${interTight.variable} h-full antialiased`}
-    >
+    <html lang="es" className={`${interTight.variable} h-full antialiased`}>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
 }
-
